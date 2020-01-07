64e50b60f909b55a59b924b6ab8be72d4607d2db
jbennett7 <joe.bennett@contino.io>
2020-01-06 23:51:27 -0500

    reformats main.tf and variable.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #TODO: Bastion host should be in an autoscaling  group of one in two availability zones.
 
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = var.profile
 }
 
@@ -16,49 +16,49 @@ data "aws_availability_zones" "available" {
 }
 
 data "aws_ami" "bastion_ami" {
-  owners = ["amazon"]
+  owners      = ["amazon"]
   most_recent = true
   filter {
-    name = "name"
+    name   = "name"
     values = var.ami_filter_name_values
   }
 }
 
 module "vpc" {
-  source = "github.com/terraform-aws-modules/terraform-aws-vpc"
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-  azs = slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones - 1)
-  private_subnets = var.private_subnets
-  public_subnets = var.public_subnets
+  source             = "github.com/terraform-aws-modules/terraform-aws-vpc"
+  name               = var.vpc_name
+  cidr               = var.vpc_cidr
+  azs                = slice(data.aws_availability_zones.available.names, 0, var.number_of_availability_zones - 1)
+  private_subnets    = var.private_subnets
+  public_subnets     = var.public_subnets
   enable_nat_gateway = var.enable_nat_gateway
   enable_vpn_gateway = var.enable_vpn_gateway
-  tags = var.tags
+  tags               = var.tags
 }
 
 module "bastion_key_pair" {
-  source = "github.com/terraform-aws-modules/terraform-aws-key-pair"
-  key_name = "bastion_host"
+  source     = "github.com/terraform-aws-modules/terraform-aws-key-pair"
+  key_name   = "bastion_host"
   public_key = file(var.public_key_path)
 }
 
 module "bastion_security_group" {
-  source = "github.com/terraform-aws-modules/terraform-aws-security-group//modules/ssh"
-  name = "bastion-security-group"
-  description = "Security group for the bastion host."
-  vpc_id = module.vpc.vpc_id
+  source              = "github.com/terraform-aws-modules/terraform-aws-security-group//modules/ssh"
+  name                = "bastion-security-group"
+  description         = "Security group for the bastion host."
+  vpc_id              = module.vpc.vpc_id
   ingress_cidr_blocks = var.bastion_ingress_cidr_blocks
 }
 
 module "bastion_host" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
-  name = var.bastion_name
-  ami = data.aws_ami.bastion_ami.id
-  instance_count = 1
-  instance_type = var.bastion_host_instance_type
-  key_name = module.bastion_key_pair.this_key_pair_key_name
-  monitoring = var.monitoring
+  source                 = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+  name                   = var.bastion_name
+  ami                    = data.aws_ami.bastion_ami.id
+  instance_count         = 1
+  instance_type          = var.bastion_host_instance_type
+  key_name               = module.bastion_key_pair.this_key_pair_key_name
+  monitoring             = var.monitoring
   vpc_security_group_ids = [module.bastion_security_group.this_security_group_id]
-  subnet_id = module.vpc.public_subnets[0]
-  tags = var.tags
+  subnet_id              = module.vpc.public_subnets[0]
+  tags                   = var.tags
 }
--- variable.tf
+++ variable.tf
@@ -1,93 +1,93 @@
 variable "ami_filter_name_values" {
-  type = list(string)
-  default = ["amzn2-ami-hvm-*-x86_64-*"]
+  type        = list(string)
+  default     = ["amzn2-ami-hvm-*-x86_64-*"]
   description = "The filter to apply when selecting the ami."
 }
 
 variable "ami_owners" {
-  type = list(string)
-  default = ["amazon"]
+  type        = list(string)
+  default     = ["amazon"]
   description = "The owner of the ami to use when deploying the bastion host."
 }
 
 variable "bastion_host_instance_type" {
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
   description = "The instance type used for the bastion host."
 }
 
 variable "bastion_ingress_cidr_blocks" {
-  type = list
+  type        = list
   description = "The bastion host ingress cidr to apply for ssh access."
 }
 
 variable "bastion_name" {
-  type = string
+  type        = string
   description = "The name of the bastion host."
 }
 
 variable "enable_nat_gateway" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable a NAT gateway."
 }
 
 variable "enable_vpn_gateway" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable a VPN gateway."
 }
 
 variable "monitoring" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable monitoring for the bastion host."
 }
 
 variable "number_of_availability_zones" {
-  type = number
+  type        = number
   description = "The number of availability zones to use."
 }
 
 variable "private_subnets" {
-  type = list(string)
+  type        = list(string)
   description = "The private subnet cidr block."
 }
 
 variable "profile" {
-  type = string
-  default = "default"
+  type        = string
+  default     = "default"
   description = "The profile to use when deploying the resources."
 }
 
 variable "public_key_path" {
-  type = string
+  type        = string
   description = "SSH public key path."
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type        = list(string)
   description = "The public subnet cidr block."
 }
 
 variable "region" {
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
   description = "The region to deploy the resources in."
 }
 
 variable "tags" {
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
   description = "Tags to assign to all resources used."
 }
 
 variable "vpc_cidr" {
-  type = string
+  type        = string
   description = "Ensure the CIDR block reserved for this VPC does not overlap with other networks."
 }
 
 variable "vpc_name" {
-  type = string
+  type        = string
   description = "Name of the VPC for the environment."
 }
